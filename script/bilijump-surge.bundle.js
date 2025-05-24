d45a157f7132cd0756ff3e637f38824ad6ae7f24
qingmeng <qingmeng@live.cn>
2025-05-25 01:01:10 +0800

    update

--- script/bilijump-surge.bundle.js
+++ script/bilijump-surge.bundle.js
@@ -4629,6 +4629,7 @@
         rawBody[0] = 0; // 直接置protobuf 为未压缩状态
         rawBody.set(checksum, 1); // 1-4位： 校验值(4位)
         rawBody.set(body, 5); // 5-end位：protobuf数据
+        console.log(`${cid}: ${JSON.stringify(segments)}`);
         return rawBody;
     }
     function Checksum(num) {
